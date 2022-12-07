cc066202b53171e7f8033b0e44d3b8092a45cdff
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2022-12-07 19:24:35 -0300

    aulas 1-8

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+/* alert("Olá mundo! 1.0");
+
+console.log("Olá mundo! 3.0");
+
+
+let nome = "Santhiago"; // string
+let idade = 20; // number
+let cidade = "Umuarama"; 
+let estado = "Paraná";
+let logado = true; // boolean
+let lista = ["ovo", "farinha", "corante", "açucar"]; // array
+let nomeCompleto = {nome: "libatea", idade: 324, sobrenome:"letacotapa"} // object
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(estado);
+
+*/
