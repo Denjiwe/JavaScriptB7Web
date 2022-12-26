eacb27a0dee6e4b40be8377cd7b4380d633ff3d9
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2022-12-26 11:12:49 -0300

    aulas 1-3 do módulo 8

---
+++ assets/js/modulo8.js
@@ -0,0 +1,11 @@
+let kkkk = "salve mano";
+
+console.log (kkkk.indexOf('mano'));
+
+let yay = kkkk.substring(5, 10);
+
+console.log (yay);
+
+let amem = kkkk.replace('mano', 'amem');
+
+console.log(amem.charAt(6) + ' ' + amem);
