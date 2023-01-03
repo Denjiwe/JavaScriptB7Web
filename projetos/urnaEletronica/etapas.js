3a4867326afa1c697c74f28df5b7a45e4ec68693
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2023-01-02 21:39:00 -0300

    ADD projeto urna eletrônica

---
+++ projetos/urnaEletronica/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo : 'VEREADOR',
+        numeros: 5,
+        candidatos: [ 
+        {
+            numero: '38111',
+            nome: 'Fulano de tal',
+            partido: 'ABC',
+            fotos: [
+                { url: 'images/astapfp2.jpg', legenda: 'Vereador'}
+            ]
+        },
+        {
+            numero: '772222',
+            nome: 'Beltrano da Silva',
+            partido: 'DEFG',
+            fotos: [
+                {url: 'images/astapfp2.jpg', legenda: 'Vereador'}
+            ]
+        }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Cicladno',
+                partido: 'ABC',
+                vic: 'Cic',
+                fotos: [
+                    {url: 'images/hajime.png', legenda: 'Prefeito'},
+                    {url: 'images/hajime2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '98',
+                nome: 'Zulano',
+                partido: 'DEFG',
+                vic: 'Zul',
+                fotos: [
+                    {url: 'images/foto_de_perfil_santhiago.png', legenda: 'Prefeito'},
+                    {url: 'images/Magna.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+]
